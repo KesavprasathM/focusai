1f2cedea1f6258a069879ccd522e2205f4348012
Kesav <mkesavprasath@gmail.com>
2026-03-19 23:13:06 +0530

    Fix: add set -e to build.sh to catch torch install errors

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pip install torch==2.9.0+cpu torchvision --index-url https://download.pytorch.org/whl/cpu
+set -e
+pip install torch==2.3.1+cpu torchvision==0.18.1+cpu --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
