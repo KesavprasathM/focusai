7ec2fee9548a8ca0eb1d218f425a39fb6ab7b6de
Kesav <mkesavprasath@gmail.com>
2026-03-19 23:02:31 +0530

    Fix: update torch version for Python 3.14

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install torch==2.3.1+cpu torchvision==0.18.1+cpu --index-url https://download.pytorch.org/whl/cpu
+pip install torch==2.9.0+cpu torchvision --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
