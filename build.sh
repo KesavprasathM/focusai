c52ffba1426856d37a07e2bcc609bef9dade03df
Kesav <mkesavprasath@gmail.com>
2026-03-22 15:45:11 +0530

    Fix torch/torchvision version conflict for Railway

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-pip install torch==2.9.0+cpu torchvision==0.25.0+cpu --index-url https://download.pytorch.org/whl/cpu
+pip install torch==2.9.0+cpu torchvision==0.24.0+cpu --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
