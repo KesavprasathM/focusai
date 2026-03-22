33b8119f8afa1b86f0ab69f222d7f73d391ba2b2
Kesav <mkesavprasath@gmail.com>
2026-03-22 16:18:39 +0530

    fix: use dynamic PORT for Railway

--- app.py
+++ app.py
@@ -794,5 +794,5 @@ if __name__ == '__main__':
 """)
     model_manager.load()
     # socketio.run(app, host='0.0.0.0', port=5000, debug=False)
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     socketio.run(app, host='0.0.0.0', port=port, debug=False)
