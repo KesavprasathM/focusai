cdf0f6e55f1cb35a7d2a45d1537c5a4f2862f916
Kesav <mkesavprasath@gmail.com>
2026-03-22 16:11:24 +0530

    Force eventlet worker via GUNICORN_CMD_ARGS in build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ set -e
 
 pip install torch==2.9.0+cpu torchvision==0.24.0+cpu --index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
+
+# Force eventlet worker for SocketIO support
+export GUNICORN_CMD_ARGS="--worker-class eventlet"
